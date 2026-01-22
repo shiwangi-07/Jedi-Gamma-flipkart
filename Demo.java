a37562779c3a2c9520828889b93d670457203477
Gulshan Kumar Gupta <coding.messiah007@gmail.com>
2026-01-22 16:19:40 +0530

    Commit by Gulshan

---
+++ Demo.java
@@ -0,0 +1,14 @@
+public class Demo {
+    public static void main(String[] args) {
+        // This is your main block
+        System.out.println("Hello, World!");
+
+        int a = 10;
+        int b = 20;
+        int result = a + b;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + result);
+    }
+}
+
+
